de99270456a966c9fa5f9f716455cde39e2dcfec
yabloco <none>
2025-06-17 19:30:56 +0300

    edit similarity

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import re
 
 import numpy as np
+from fuzzywuzzy import fuzz
 
 
 def exact_match(prediction, ground_truth):
@@ -18,3 +19,15 @@ def estimate_pass_at_k(n: int, c: int, k: int) -> float:
     if n - c < k:
         return 1.0
     return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+
+def edit_similarity(predictions, ground_truths):
+    if not predictions:
+        return 0
+
+    edit_sim = 0.0
+    for pred, gt in zip(predictions, ground_truths):
+        edit_sim += fuzz.ratio(pred, gt)
+
+    return round(edit_sim / len(predictions), 2)
+
